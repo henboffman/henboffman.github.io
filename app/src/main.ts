09bbe6f031de106617863d9d6e7e81c9a7ccc9ea
Ben Hoffman <benbrent@gmail.com>
2021-07-13 22:37:39 -0400

    fixed buttons, layout

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,20 +2,23 @@ import { Aurelia } from 'aurelia-framework';
 import * as environment from '../config/environment.json';
 import 'bootstrap'; // Import the Javascript
 import 'bootstrap/dist/css/bootstrap.css'; // Import the CSS
+import "bootstrap/dist/js/bootstrap";
 import "@fortawesome/fontawesome-free";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "jquery";
+import "popper.js";
 import { PLATFORM } from 'aurelia-pal';
 
 export function configure(aurelia: Aurelia): void {
-  aurelia.use
-    .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+    aurelia.use
+        .standardConfiguration()
+        .feature(PLATFORM.moduleName('resources/index'));
 
-  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+    aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
-  if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
-  }
+    if (environment.testing) {
+        aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('routes/shell/shell')));
+    aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('routes/shell/shell')));
 }
